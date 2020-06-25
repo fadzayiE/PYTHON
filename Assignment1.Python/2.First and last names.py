355dfed5d5df3f62c3ff08a1a9449e25530292fc
fadzayiE <64942751+fadzayiE@users.noreply.github.com>
2020-06-24 23:12:14 -0400

    Add files via upload

---
+++ Assignment1.Python/2.First and last names.py
@@ -0,0 +1,5 @@
+first = input("Enter First name:")
+last = input("Enter Last name:")
+f = first[::-1]
+l = last[::-1]
+print(" "+ f +" "+l)
---
+++ Assignment1.Python/3.Volume of Sphere.py
@@ -0,0 +1,9 @@
+pi = 22/7
+radian = float(input("Radian of Sphere:"))
+volume = (4/3) * (pi * radian **3)
+print("Volume is:" ,volume)
+
+
+
+
+
